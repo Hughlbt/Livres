0c2d66f093b369aa5248d5f77e7fd71927084dc8
Hugues <hugues.poncet@yahoo.fr>
2025-01-27 16:57:16 +0100

    Optimisation de la méthode put pour modifier un livre avec multer

--- controllers/book.js
+++ controllers/book.js
@@ -18,9 +18,25 @@ exports.addBook = (req, res, next) => {
         .catch(error => { res.status(400).json({ error }) });
 };
 exports.modifyBook = (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Livre modifié !' }))
-        .catch(error => res.status(400).json({ error }));
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    delete bookObject._userId;
+    Book.findOne({ _id: req.params.id })
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Not authorized' });
+            } else {
+                Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Livre modifié!' }))
+                    .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
 };
 
 exports.deleteBook = (req, res, next) => {
--- routes/stuff.js
+++ routes/stuff.js
@@ -5,7 +5,7 @@ const bookCtrl = require('../controllers/book');
 const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, bookCtrl.addBook);
-router.put('/:id', auth, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', auth, bookCtrl.getOneBook);
 router.get('/', auth, bookCtrl.getAllBooks);
